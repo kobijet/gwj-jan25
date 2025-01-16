ba66cd4969e5066930fbd0d98211d1001d116c72
kobijet <kobijet@gmail.com>
2025-01-16 02:11:14 -0800

    fix(ranged weapon): Bullets deal damage when they hit targets and destroy themselves after 1.5 seconds

--- game/scripts/weapons/Bullet.cs
+++ game/scripts/weapons/Bullet.cs
@@ -9,14 +9,21 @@ public partial class Bullet : Area2D
 	
 	public Vector2 direction;
 
+	public override void _Ready()
+	{
+		this.Connect( SignalName.BodyEntered, Callable.From((Node2D body) => OnBodyEntered(body)) );
+	}
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		Position += direction * speed * (float)delta;
 	}
 
-	public void OnBodyEntered()
+	public void OnBodyEntered(Node2D body)
 	{
-		GD.Print("Hit!");
+		QueueFree();
+		HealthComponent healthComponent = body.GetNode<HealthComponent>("HealthComponent");
+		healthComponent.TakeDamage(damage);
 	}
 }
