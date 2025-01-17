94f87a39c4541e711f248ed3373b81e92f3c9893
piercingecho <piercem4502@gmail.com>
2025-01-16 22:43:21 -0500

    Add Game Over UI and extra UI for title screen

---
+++ game/scripts/game_over/game_over.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_restart_button_pressed():
+	SceneManager.to_gameplay()
+
+
+
+func _on_title_button_pressed():
+	SceneManager.to_main_menu()
